c71d7f06e8a3a8b87a927242c1d8437fee6a8ead
Amir khan <amirkhan1092@gmail.com>
2020-04-01 19:36:01 +0530

    website and user entry

--- web_user_pair.py
+++ web_user_pair.py
@@ -6,7 +6,9 @@
 # Then a reasonable similarity metric would most likely conclude that a and e are the most similar, so your program should return [('a', 'e')].
 
 from itertools import combinations
-data = eval(input())
+data = eval(input())  # enter the data list web user pair
+cmp = int(input())  # enter number of pairs k = 1
+
 match = None
 d = {}
 for i, j in data:
@@ -21,7 +23,8 @@ for y, k in combinations(d.keys(), 2):
 
 # print(res)
 p = min(res, key=lambda x:x[1])
-print(p[0])
+for i in range(cmp):
+    print(p[i])
 
 
 # [('a', 1), ('a', 3), ('a', 5),  ('b', 2), ('b', 6),  ('c', 1), ('c', 2), ('c', 3), ('c', 4), ('c', 5) , ('d', 4), ('d', 5), ('d', 6), ('d', 7),  ('e', 1), ('e', 3), ('e', 5), ('e', 6)]

c41ccf2eb4c43e895024f50e4ed5a4a193886d65
Amir khan <amirkhan1092@gmail.com>
2020-04-01 19:26:06 +0530

    website and user entry

---
+++ web_user_pair.py
@@ -0,0 +1,27 @@
+
+# This problem was asked by Quora.
+# You are given a list of (website, user) pairs that represent users visiting websites. Come up with a program that identifies the top k pairs of websites with the greatest similarity.
+# For example, suppose k = 1, and the list of tuples is:
+# [('a', 1), ('a', 3), ('a', 5),  ('b', 2), ('b', 6),  ('c', 1), ('c', 2), ('c', 3), ('c', 4), ('c', 5)  ('d', 4), ('d', 5), ('d', 6), ('d', 7),  ('e', 1), ('e', 3), ('e', 5), ('e', 6)]
+# Then a reasonable similarity metric would most likely conclude that a and e are the most similar, so your program should return [('a', 'e')].
+
+from itertools import combinations
+data = eval(input())
+match = None
+d = {}
+for i, j in data:
+    if i == match:
+        d[i].append(j)
+    else:
+        d[i] = [j]
+        match = i
+res = []
+for y, k in combinations(d.keys(), 2):
+    res.append(([y, k], len(set(d[y]) ^ set(d[k]))))
+
+# print(res)
+p = min(res, key=lambda x:x[1])
+print(p[0])
+
+
+# [('a', 1), ('a', 3), ('a', 5),  ('b', 2), ('b', 6),  ('c', 1), ('c', 2), ('c', 3), ('c', 4), ('c', 5) , ('d', 4), ('d', 5), ('d', 6), ('d', 7),  ('e', 1), ('e', 3), ('e', 5), ('e', 6)]

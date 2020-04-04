83f1d79361f0df7338e58f413202e1e7b749f8c9
Amir khan <amirkhan1092@gmail.com>
2020-04-04 23:16:31 +0530

    python38-05 solutions

---
+++ color_combination.py
@@ -0,0 +1,22 @@
+
+
+k = eval(input("enter the list of colors ")) # ["R", "G", "B", "R", "G", "R"]
+st = {'R', 'G', 'B'}
+i = 0
+lst = []
+print(k)
+while len(k)> 1:
+    if len(k) >= 2:
+        while 1:
+            s = set([k[i], k[i+1]])
+            if len(s) > 1:
+                print(s, end = '---> ')
+                l = st - set((k.pop(i), k.pop(i)))
+                k.insert(i, *l)
+                print(l, end='\t')
+                print(k)
+                break
+            else:
+                i += 1
+        i = 0
+print(f'\nOutput is "{k}"')
--- fixed_digits.py
+++ fixed_digits.py
@@ -1,4 +1,5 @@
-import time
+#
+# Harry to Hogwarts
 
 num = input('enter the digits ')
 kh = ''.join(sorted(num))
@@ -9,6 +10,3 @@ while tm != kh:
     count += 1
 
 print(count)
-
-h = 'R\x1e\x00\x00\x00\x00\x00\x00\n\xcf<\n\xf0\x02\n\x08abstract\x12\xe3\x02\n\xe0\x02\n\xdd\x02<s> marseille prosecutor says'
-print(h)

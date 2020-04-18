ef3e5ea214e6d9c32545eba3a094b0535728e3a3
amirkhan1092 <amirkhan1092@gmail.com>
2020-04-18 23:56:36 +0530

    lazy bartender solution

--- lazy_bartender.py
+++ lazy_bartender.py
@@ -1,34 +1,27 @@
 # lazy bartender
 from functools import reduce
-import itertools
+from itertools import combinations
 
 dct = eval(input())
 valu = list(dct.values())  # val = [[0, 1, 3, 6], [1, 4, 7], [2, 4, 7, 5], [3, 2, 5], [5, 8]]
-re = reduce(lambda x1, x2: set(x1) | set(x2), valu)
+l = list(reduce(lambda x1, x2: set(x1) | set(x2), valu))
 # main content
 
 # { 0: [0, 1, 3, 6], 1: [1, 4, 7], 2: [2, 4, 7, 5], 3: [3, 2, 5], 4: [5, 8]}
-l = []
-for i, j in dct.items():
-    for k in j:
-        if (k not in l):
-            l.append(k)
-br = []
-for i in range(1, len(l) + 1):
-    br.extend(set(itertools.combinations(l, i)))
-print(br)
 
+br = []
+for i in range(1, len(l) + 1):br.extend(set(combinations(l, i)))
 
 for i in br:
-     l2 = []
-     for j in i:
-         for k in dct.keys():
-             if (j in dct[k]):
-                 if (j in l):
-                     l2.append(k)
-             if (sorted(l2) == sorted(list(dct.keys()))):
-                 print(len(i))
-                 exit()
+    l2 = []
+    for j in i:
+        for k in dct.keys():
+            if (j in dct[k]):
+                if (j in l):
+                    l2.append(k)
+    if (sorted(l2) == sorted(list(dct.keys()))):
+        print(len(i))
+        exit()
 
 
 

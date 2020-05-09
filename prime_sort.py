4a6db0d5336cc0330200f7930f8a2cfe6465d4c3
amirkhan1092 <amirkhan1092@gmail.com>
2020-05-09 12:46:48 +0530

    solution tested

--- prime_sort.py
+++ prime_sort.py
@@ -1,6 +1,6 @@
 
-
+import sympy
 k = eval(input())
 
-out = ''.join(sorted(k, key=lambda x: int(x) if sympy.isprime(x) else 100))
+out = ''.join(sorted(k, key=lambda x: int(x) if sympy.isprime(int(x)) else 100))
 print(f'"{out}"')

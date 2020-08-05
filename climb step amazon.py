f1150ebff1ca2a43dc1c4864f6c814bcf50969b8
amirkhan1092 <amirkhan1092@gmail.com>
2020-08-05 23:45:55 +0530

    using combinations

--- climb step amazon.py
+++ climb step amazon.py
@@ -13,7 +13,23 @@ For example, if N is 4, then there are 5 unique ways:
 1, 1, 2
 2, 2
 What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
-
 '''
+
+import random
+import itertools
 N = int(input())  # number of steps
-climb = eval(input())  # set of climbs steps taken by user
+climb = eval(input())  # set of climbs steps taken by user example climb = [1, 2]
+# print(list(itertools.combinations(climb, N)))
+
+k = [[i]*N for i in climb]
+pk = [i for p in k for i in p]
+L = []
+for i in range(1, N+1):
+    for comb in itertools.combinations(pk, i):
+        if sum(comb) == N:
+            L.append(comb)
+
+
+
+
+
